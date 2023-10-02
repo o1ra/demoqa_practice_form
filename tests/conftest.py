2b0e2bbc7b68c4f8c0b309825f8e6ef2e4109935
Irina Kirillova <tokkirin@gmail.com>
2023-10-02 15:40:56 +0600

    fix 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ def browser_open():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+
     yield
 
     browser.quit()
