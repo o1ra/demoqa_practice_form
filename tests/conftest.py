d25e8790d92ebd1c1eefc544bf3777b5c6c8a26e
Irina Kirillova <tokkirin@gmail.com>
2023-10-02 15:40:35 +0600

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
 import pytest
 from selene import browser
+from selene.support import webdriver
 from selenium.webdriver.chrome import options
 
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_open():
     browser.config.base_url = 'https://demoqa.com'
-    # browser.config.window_width = 1920
-    # browser.config.window_height = 1080
-    # options.headless = True
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield
 
-    # yield
-    #
-    # browser.quit()
+    browser.quit()
