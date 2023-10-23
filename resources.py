0086242d3a198946b5e2f1cbd5cf7cfbb826c925
Irina Kirillova <tokkirin@gmail.com>
2023-10-23 13:42:16 +0600

    fix resources 2

--- resources.py
+++ resources.py
@@ -1,6 +1,5 @@
 import os
 
 
-
 def path(file_name):
-    return str(os.path.abspath(os.path.join(os.path.dirname(__file__),f'tests/resources/img/{file_name}')))
+    return str(os.path.abspath(os.path.join(os.path.dirname(__file__), f'tests/resources/img/{file_name}')))
