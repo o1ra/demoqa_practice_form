32036aa521b8f7c506e0e209d66414d56fea6f8e
Irina Kirillova <tokkirin@gmail.com>
2023-10-29 19:54:22 +0600

    jenkins

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,23 +1,29 @@
+import allure
+
 from demoqa_tests.pages.registration_page import RegistrationPage
 from demoqa_tests.data import users_data
 
 
+@allure.title("Successful fill form")
 def test_practice_form():
     registration_page = RegistrationPage()
 
-    registration_page.open()
+    with allure.step("Open registrations form"):
+        registration_page.open()
 
-    registration_page.fill_full_name(users_data.irina)
-    registration_page.fill_email(users_data.irina)
-    registration_page.gender(users_data.irina)
-    registration_page.fill_number(users_data.irina)
-    registration_page.fill_date_of_birth(users_data.irina)
-    registration_page.fill_subject(users_data.irina)
-    registration_page.fill_hobbies(users_data.irina)
-    registration_page.fill_image(users_data.irina)
-    registration_page.fill_address(users_data.irina)
-    registration_page.fill_state_and_city(users_data.irina)
-    registration_page.click_button()
+    with allure.step("Fill form"):
+        registration_page.fill_full_name(users_data.irina)
+        registration_page.fill_email(users_data.irina)
+        registration_page.gender(users_data.irina)
+        registration_page.fill_number(users_data.irina)
+        registration_page.fill_date_of_birth(users_data.irina)
+        registration_page.fill_subject(users_data.irina)
+        registration_page.fill_hobbies(users_data.irina)
+        registration_page.fill_image(users_data.irina)
+        registration_page.fill_address(users_data.irina)
+        registration_page.fill_state_and_city(users_data.irina)
+        registration_page.click_button()
 
-    registration_page.should_have_registrated_user_with(users_data.irina)
-    registration_page.close_modal()
+    with allure.step("Check form results"):
+        registration_page.should_have_registrated_user_with(users_data.irina)
+        registration_page.close_modal()
