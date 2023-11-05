6031a9760abe0dd43384ec299d0e47af6ce9427e
Irina Kirillova <tokkirin@gmail.com>
2023-11-05 21:53:55 +0600

    fix conftest

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = (
