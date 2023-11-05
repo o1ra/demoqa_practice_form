4ebfea7c72526e021007ab859cb1ca4625418356
Irina Kirillova <tokkirin@gmail.com>
2023-11-05 22:29:56 +0600

    fix conftest

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import Config, browser
 from dotenv import load_dotenv
 
 from utils import attach
@@ -41,7 +41,7 @@ def setup_browser(request):
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options,
     )
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
 
     yield browser
 
